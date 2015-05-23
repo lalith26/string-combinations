ae5ffabb1c0d73aa1693fd58ff90f812bdc45d94
labr <labr@paypal.com>
2015-05-23 14:30:57 +0530

    generating combinations

---
+++ index.js
@@ -0,0 +1,28 @@
+module.exports = {
+  generate:generate
+}
+
+
+function generate(input){
+  inputString = input.split('');
+  //calculate the length of string
+  var inputStringLength = inputString.length;
+  //the number of possible combinations
+  var noOfCombinations  = Math.pow(2, inputStringLength);
+  //the result array
+  var combinations = [];
+  //the logic
+  var tempString = "";
+  for(var i = 0; i < noOfCombinations; i++){
+    tempString = "";
+    for(var j = 0; j < inputStringLength; j++){
+      if(i & Math.pow(2, j)){
+        tempString += inputString[j];
+      }
+    }
+    if(tempString != "")
+      combinations.push(tempString);
+  }
+  console.log(combinations);
+  return combinations;
+}
